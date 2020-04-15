3c99625200fb96074fe2117baafb3a0e67c0e486
NRenukeshwar <noolurenu.97@gmail.com>
2020-04-16 01:41:08 +0530

    6 files modified

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -2,27 +2,27 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Employee } from '../employee-data';
 import { DataService} from '../employee.service'
 import {Router} from '@angular/router';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'add',
   templateUrl: './add.component.html'
 })
 export class AddEmployeeComponent {
-   employee: Employee;
+   employee:Employee;
   constructor( private dataService: DataService, private router:Router){};
-
+  valid:false;
   goBack(): void {
     this.router.navigate(['/employees']);
   }
-   add(name,location,email,mobile)
+   add(f: NgForm)
    {
-     this.employee={name:"",location:"",email:"",mobile:""};
-     this.employee.name=name;
-     this.employee.location=location;
-     this.employee.email=email;
-     this.employee.mobile=mobile;
+    
+     this.employee=f.value;
+     
      console.log(this.employee);
     
      this.dataService.addEmployee(this.employee).subscribe(() => this.goBack());
+   
    }
+
 }
